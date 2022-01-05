6f843ae3189eed92d5979414c68f569c3098c795
laurenmnelson <46546796+laurenmnelson@users.noreply.github.com>
2022-01-05 15:00:10 -0800

    added icon switches with js

---
+++ app.js
@@ -0,0 +1,29 @@
+function onHoverL()
+{
+    document.getElementById("linkedin").src = "/img/linkedin_color.png";
+}
+
+function offHoverL()
+{
+    document.getElementById("linkedin").src = "/img/linkedin.png";
+}
+
+function onHoverGH()
+{
+    document.getElementById("github").src = "/img/github_color.png";
+}
+
+function offHoverGH()
+{
+    document.getElementById("github").src = "/img/github.png";
+}
+
+function onHoverMail()
+{
+    document.getElementById("mail").src = "/img/mail_open.png";
+}
+
+function offHoverMail()
+{
+    document.getElementById("mail").src = "/img/mail.png";
+}
